a16ea5038a9ca80ddcdf0562f465950bbb47a524
specterpatronus-blip <specterpatronus@gmail.com>
2026-03-07 10:58:11 -0500

    V0.2 BETA

--- ios/Runner/MetadataExtractor.swift
+++ ios/Runner/MetadataExtractor.swift
@@ -12,34 +12,33 @@ class MetadataExtractor {
         
         var metadataDict: [String: Any] = [:]
         
-        // Use loadValuesAsynchronously for modern iOS (we use Task for async/await)
-        Task {
-            do {
-                let duration = try await asset.load(.duration)
-                metadataDict["duration"] = duration.seconds
+        let keys = ["duration", "commonMetadata"]
+        asset.loadValuesAsynchronously(forKeys: keys) {
+            var metadataDict: [String: Any] = [:]
+            
+            var error: NSError? = nil
+            let status = asset.statusOfValue(forKey: "commonMetadata", error: &error)
+            
+            if status == .loaded {
+                metadataDict["duration"] = asset.duration.seconds
                 
-                let commonMetadata = try await asset.load(.commonMetadata)
-                for item in commonMetadata {
-                    if item.commonKey == .commonKeyTitle, let title = try await item.load(.stringValue) {
+                for item in asset.commonMetadata {
+                    if item.commonKey == .commonKeyTitle, let title = item.stringValue {
                         metadataDict["title"] = title
-                    } else if item.commonKey == .commonKeyArtist, let artist = try await item.load(.stringValue) {
+                    } else if item.commonKey == .commonKeyArtist, let artist = item.stringValue {
                         metadataDict["artist"] = artist
-                    } else if item.commonKey == .commonKeyArtwork, let data = try await item.load(.dataValue) {
-                        if let imagePath = saveArtworkLocally(data: data) {
+                    } else if item.commonKey == .commonKeyArtwork, let data = item.dataValue {
+                        if let imagePath = self.saveArtworkLocally(data: data) {
                             metadataDict["artworkPath"] = imagePath
                         }
                     }
                 }
-                
-                DispatchQueue.main.async {
-                    completion(metadataDict)
-                }
-                
-            } catch {
-                print("MetadataExtractor: Failed to load metadata for \(filePath): \(error)")
-                DispatchQueue.main.async {
-                    completion(metadataDict)
-                }
+            } else {
+                print("MetadataExtractor: Failed to load metadata for \(filePath): \(String(describing: error))")
+            }
+            
+            DispatchQueue.main.async {
+                completion(metadataDict)
             }
         }
     }
